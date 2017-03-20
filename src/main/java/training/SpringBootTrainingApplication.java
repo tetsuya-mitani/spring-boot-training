76c44b394dcb314e3b7453bf19d414007ddbd2bb
mitanitetsuya <mitani0328@hotmail.co.jp>
2017-03-20 15:24:16 +0900

    initial commit

---
+++ src/main/java/training/SpringBootTrainingApplication.java
@@ -0,0 +1,12 @@
+package training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTrainingApplication.class, args);
+	}
+}
